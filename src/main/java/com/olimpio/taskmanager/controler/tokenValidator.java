fbed065818bbaf76c12514c3d9565d2cf2c70733
Guilherme Olimpio <guilhermearnaldi@gmail.com>
2026-07-13 21:32:28 -0300

    ConcertFilterToken
    Filter token ainda não esta 100%, ja terminei o back, falta o front

---
+++ src/main/java/com/olimpio/taskmanager/controler/tokenValidator.java
@@ -0,0 +1,24 @@
+package com.olimpio.taskmanager.controler;
+
+import com.olimpio.taskmanager.util.JwtUtil;
+import io.jsonwebtoken.Claims;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/validator")
+public class tokenValidator {
+
+    private String token;
+    private final JwtUtil jwtUtil;
+
+    public tokenValidator(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping
+    @CrossOrigin("http://localhost:5173")
+    public Claims returnValitator(@RequestBody String token){
+        return jwtUtil.validarToken(token);
+    }
+
+}
--- src/main/java/com/olimpio/taskmanager/service/FilterToken.java
+++ src/main/java/com/olimpio/taskmanager/service/FilterToken.java
@@ -1,9 +1,11 @@
 package com.olimpio.taskmanager.service;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Objects;
 
 import com.olimpio.taskmanager.util.JwtUtil;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -12,12 +14,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 @Component
 public class FilterToken extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
 
+
     public FilterToken(JwtUtil jwtUtil) {
+
         this.jwtUtil = jwtUtil;
     }
 
@@ -27,22 +34,29 @@ public class FilterToken extends OncePerRequestFilter {
                                     FilterChain filterChain) throws ServletException, IOException {
 
         if (!Objects.equals(request.getRequestURI(), "/login")) {
+            if (!Objects.equals(request.getRequestURI(), "/validator")) {
 
-            String authHeader = request.getHeader("Authorization");
-            if (authHeader == null) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
+                String authHeader = request.getHeader("Authorization");
+                if (authHeader == null) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    return;
+                }
 
-            String token = authHeader.substring("Bearer ".length());
+                String token = authHeader.substring("Bearer ".length());
 
 
-            try {
-                jwtUtil.validarToken(token);
+                try {
+                    Claims nameFromToken = jwtUtil.validarToken(token);
+                    String nameFromTokenSubjected = nameFromToken.getSubject();
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(nameFromTokenSubjected, null, Collections.emptyList());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                    filterChain.doFilter(request, response);
+                } catch (SignatureException e) {
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                }
 
+            } else {
                 filterChain.doFilter(request, response);
-            } catch (SignatureException e) {
-                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             }
         } else {
             filterChain.doFilter(request, response);
