db26b71376002eb35bb346b58658627c1fca8f22
Guilherme Olimpio <guilhermearnaldi@gmail.com>
2026-07-22 19:26:22 -0300

    UISignFinal
    É só licha e pinta

--- frontend/src/components/Sign.jsx
+++ frontend/src/components/Sign.jsx
@@ -1,22 +1,87 @@
 import { useNavigate } from "react-router-dom"
+import { useState, useRef, useEffect } from "react"
 
-import a1 from "../assets/SignAside/a1.jpg"
-import a2 from "../assets/SignAside/a2.png"
-import a3 from "../assets/SignAside/a3.png"
-import { useState } from "react"
 
+import a1 from '../assets/SignAside/a1.jpg'
+import a2 from '../assets/SignAside/a2.png'
+import a3 from '../assets/SignAside/a3.png'
 
 function Sign(){
     const navigate = useNavigate()
-    const imagesDiv = [a1, a2, a3] = useState([a1, a2, a3])
+    const img1Aside = useRef(null)
+    const img2Aside = useRef(null)
+    const img3Aside = useRef(null)
+
+    const firstNameRef = useRef(null)
+    const lastNameRef = useRef(null)
+    const usernameRef = useRef(null)
+    const emailRef = useRef(null)
+    const passwordRef = useRef(null)
+    const AgreeTermsRef = useRef(null)
+
+    const errorTermsRef = useRef(null)
+    const errorUsernameRef = useRef(null)
+    const errorEmailRef = useRef(null)
+    const errorPasswordRef = useRef(null)
+
+    const [temp, setTemp] = useState(0)
+
+    useEffect(() => {
+        if (img1Aside.current && img2Aside.current && img3Aside.current) {
+
+            const timeout = setTimeout(() => { setTemp(prev => prev + 1) }, 10000)
+            if (temp === 0) {
+                img1Aside.current.classList.remove("translate-x-full")
+                img2Aside.current.classList.remove("translate-x-full")
+                img3Aside.current.classList.remove("translate-x-full")
+            } else if (temp === 1) {
+                img3Aside.current.classList.add("translate-x-full")
+            } else if (temp === 2) {
+                img2Aside.current.classList.add("translate-x-full")
+            } else {
+                img1Aside.current.classList.add("translate-x-full")
+                setTemp(0)
+            }
+            return () => clearTimeout(timeout);
+        } else {
+            return
+        }
+        
+
+    }, [temp])
+
+    function submit(){
+        if (!AgreeTermsRef.current.checked || usernameRef.current.value === "" || emailRef.current.value === "" || passwordRef.current.value === ""){
+            if(!AgreeTermsRef.current.checked){
+                errorTermsRef.current.classList.add("opacity-100")
+                errorTermsRef.current.classList.remove("opacity-0")
+            }
+            if (usernameRef.current.value === ""){
+                errorUsernameRef.current.classList.add("opacity-100")
+                errorUsernameRef.current.classList.remove("opacity-0")
+            }
+            if (emailRef.current.value === ""){
+                errorEmailRef.current.classList.add("opacity-100")
+                errorEmailRef.current.classList.remove("opacity-0")
+            }
+            if (passwordRef.current.value === ""){
+                errorPasswordRef.current.classList.add("opacity-100")
+                errorPasswordRef.current.classList.remove("opacity-0")
+            }
+            
+        } else {
+            window.alert("Conta criada com sucesso!")
+            navigate('/login')
+        }
+    }
 
     return (
-        <main className="flex flex-row gap-10 p-10 h-screen min-w-11/12 m-20 mr-10 ml-10 bg-gray-900 overflow-hidden rounded-xl">
+        <main className="flex flex-row gap-10 p-10 h-screen min-w-11/12 m-20 mr-10 ml-10 bg-gray-900 overflow-hidden rounded-xl max-w-2xl">
             <article className="w-1/2 h-full rounded-xl overflow-hidden">
                 <div className="overflow-hidden w-full h-full relative">
-                    <img src={a1} alt="a1" className="absolute inset-0 w-full h-full object-cover"/>
-                    <img src={a2} alt="a2" className="absolute inset-0 w-full h-full object-cover"/>
-                    <img src={a3} alt="a3" className="absolute inset-0 w-full h-full object-cover"/>
+                    <div ref={img1Aside} className="absolute inset-0 w-full h-full object-cover bg-cover bg-center transition-all duration-300" style={{backgroundImage: `url(${a1})`}}></div>
+                    <div ref={img2Aside} className="absolute inset-0 w-full h-full object-cover bg-cover bg-center transition-all duration-300" style={{backgroundImage: `url(${a2})`}}></div>
+                    <div ref={img3Aside} className="absolute inset-0 w-full h-full object-cover bg-cover bg-center transition-all duration-300" style={{backgroundImage: `url(${a3})`}}></div>
                 </div>
             </article>
             <article className="w-1/2 h-full p-10">
@@ -26,17 +91,23 @@ function Sign(){
                     <button className="inline text-sky-500 ml-2">Entrar</button>
                 </aside>
                 <aside className="flex flex-col h-full">
-                    <div className="w-full flex gap-1 justify-between mb-10">
-                        <input type="text" name="firstName" id="firstName" placeholder="Nome" className="text-gray-200 bg-gray-900 hover:bg-gray-700 p-3 transition duration-150 rounded-xs border border-gray-800 focus:outline-none" />
-                        <input type="text" name="lastName" id="lastName" placeholder="Sobrenome" className="text-gray-200 bg-gray-900 hover:bg-gray-700 p-3 transition duration-150 rounded-xs border border-gray-800 focus:outline-none" />
+                    <div className="w-full flex gap-1 justify-between mb-5">
+                        <span></span>
+                        <input ref={firstNameRef} type="text" name="firstName" id="firstName" placeholder="Nome" className="text-gray-200 bg-gray-900 hover:bg-gray-700 p-3 transition duration-150 rounded-xs border border-gray-800 focus:outline-none w-full" />
+                        <input ref={lastNameRef} type="text" name="lastName" id="lastName" placeholder="Sobrenome" className="text-gray-200 bg-gray-900 hover:bg-gray-700 p-3 transition duration-150 rounded-xs border border-gray-800 focus:outline-none w-full" />
                     </div>
-                    <input type="email" name="email" id="email" placeholder="Email" className="text-gray-200 bg-gray-900 hover:bg-gray-700 p-3 transition duration-150 rounded-xs mb-5 border border-gray-800 focus:outline-none" />
-                    <input type="password" name="password" id="password" placeholder="Senha" className="text-gray-200 bg-gray-900 hover:bg-gray-700 p-3 transition duration-150 rounded-xs border border-gray-800 focus:outline-none" />
-                    <button className="bg-sky-500 text-white py-2 px-4 rounded-md cursor-pointer hover:bg-sky-600 transition-all mt-20">Criar conta</button>
+                    <span ref={errorUsernameRef} className="text-red-500 text-sm font-bold opacity-0 cursor-default">* Preencha seu username para prosseguir</span>
+                    <input ref={usernameRef} type="text" name="username" id="username" placeholder="Nome de usuário" className="text-gray-200 bg-gray-900 hover:bg-gray-700 p-3 transition duration-150 rounded-xs mb-5 border border-gray-800 focus:outline-none" />
+                    <span ref={errorEmailRef} className="text-red-500 text-sm font-bold opacity-0 cursor-default">* Insira seu email para prosseguir</span>
+                    <input ref={emailRef} type="email" name="email" id="email" placeholder="Email" className="text-gray-200 bg-gray-900 hover:bg-gray-700 p-3 transition duration-150 rounded-xs mb-5 border border-gray-800 focus:outline-none" />
+                    <span ref={errorPasswordRef} className="text-red-500 text-sm font-bold opacity-0 cursor-default">* Insira sua senha para prosseguir</span>
+                    <input ref={passwordRef} type="password" name="password" id="password" placeholder="Senha" className="text-gray-200 bg-gray-900 hover:bg-gray-700 p-3 transition duration-150 rounded-xs border border-gray-800 focus:outline-none" />
+                    <button className="bg-sky-500 text-white py-2 px-4 rounded-md cursor-pointer hover:bg-sky-600 transition-all mt-10" onClick={submit}>Criar conta</button>
 
                     <div className="mt-5">
-                        <input className="inline mr-3 accent-sky-500" type="checkbox" required name="AgreeTerms" id="AgreeTerms" />
+                        <input ref={AgreeTermsRef} className="inline mr-3 accent-sky-500" type="checkbox" required name="AgreeTerms" id="AgreeTerms" />
                         <label className="inline text-gray-200" htmlFor="AgreeTerms">Concordo com os <a className="text-sky-500" href="#">termos e condições</a></label>
+                        <span ref={errorTermsRef} className="text-red-500 text-sm mt-2 font-bold opacity-0 block cursor-default">* Você precisa aceitar os termos para prosseguir</span>
                     </div>
                 </aside>
             </article>
